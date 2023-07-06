c1e05b07487dfae02894c138f45a3fb968019168
Kichiro Nakamoto <santiago.ruizm40@gmail.com>
2023-07-06 15:01:10 -0500

    Addition of better coments

--- ArrayPartition.java
+++ ArrayPartition.java
@@ -1,11 +1,11 @@
-//PROBLEM --------------------------------------------------------------------------------------------------------------
+/*
+PROBLEM --------------------------------------------------------------------------------------------------------------
 
-//Given an integer array nums of 2n integers, group these integers into n pairs (a1, b1), (a2, b2), ..., (an, bn) 
-//such that the sum of min(ai, bi) for all i is maximized. Return the maximized sum.
+Given an integer array nums of 2n integers, group these integers into n pairs (a1, b1), (a2, b2), ..., (an, bn) 
+such that the sum of min(ai, bi) for all i is maximized. Return the maximized sum.
 
-
-//SOLUTION ------------------------------------------------------------------------------------------------------------
-
+SOLUTION ------------------------------------------------------------------------------------------------------------
+*/
 
 import java.util.Arrays;
 
---
+++ TwoSum.java
@@ -0,0 +1,30 @@
+/*
+PROBLEM --------------------------------------------------------------------------------------------------------------
+
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+
+SOLUTION ------------------------------------------------------------------------------------------------------------
+*/
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int  diff = 0;
+        int[] resultado = new int[2];
+
+        for(int i = 0; i < nums.length; i++){
+           diff = target - nums[i];
+           for(int j = 0; j < nums.length; j++){
+               if(diff - nums[j] == 0 && i != j ){
+                   resultado[0] = i;
+                   resultado[1] = j;
+                   break;
+               }
+           }     
+        }
+
+    return resultado; 
+
+    } 
+}
