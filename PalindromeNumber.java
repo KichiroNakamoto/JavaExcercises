76a6634dc619b4c6cf7e50818e5d2fd6b24490fc
Kichiro Nakamoto <santiago.ruizm40@gmail.com>
2023-07-06 15:26:45 -0500

    Upload PalindomreProblem

---
+++ PalindromeNumber.java
@@ -0,0 +1,27 @@
+/*
+PROBLEM --------------------------------------------------------------------------------------------------------------
+
+Given an integer x, return true if x is a palindrome, and false otherwise.
+
+SOLUTION ------------------------------------------------------------------------------------------------------------
+*/
+
+
+class Solution {
+    public boolean isPalindrome(int x) {
+
+        String numero = String.valueOf(x);
+        char[] list = numero.toCharArray();
+        int n = list.length;
+        boolean state = true;
+
+        for(int i = 0; i < n/2; i++){
+            if(list[i] != list[n-1-i]){
+                state = false;
+                break;
+            }
+        }
+
+        return state;
+    }
+}
