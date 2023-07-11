28ceb8c3f7a635ffd62fae008f4dc88401df5579
Kichiro Nakamoto <santiago.ruizm40@gmail.com>
2023-07-11 14:18:57 -0500

    Addding problem

---
+++ LongestCommonPrefix.java
@@ -0,0 +1,50 @@
+class Solution {
+
+    public String FindLongest(String[] stringList){
+        String longest = stringList[0];
+
+        for(int k = 0; k < stringList.length; k++){
+            if (longest.length() <= stringList[k].length()){
+                longest = stringList[k];
+            }
+        }
+
+        System.out.println(longest);
+        return longest;
+    }
+
+
+    public String longestCommonPrefix(String[] strs) {
+
+        List<Character> prefix = new ArrayList<>();
+
+        char[] reference = FindLongest(strs).toCharArray();
+        String finalStr = "";
+
+        for(int i = 1; i <= strs.length - 1; i++){
+            
+            char[] palabraList = strs[i].toCharArray();
+
+                for (int j = 0; j < palabraList.length; j++){
+                    if ( reference[j] == palabraList[j]){
+                        System.out.println(reference[j]);
+                        prefix.add(reference[j]);
+
+                    }
+                }
+               
+                           
+        }
+
+
+        StringBuilder builder = new StringBuilder();
+        for(Character c : prefix){
+            builder.append(c);
+        }
+
+        String prefixBuilder =  builder.toString();
+
+        return prefixBuilder;
+
+    }
+}
