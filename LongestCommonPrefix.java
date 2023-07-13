9f900890d878ff691d54c7486b1cb211cb504092
Kichiro Nakamoto <santiago.ruizm40@gmail.com>
2023-07-12 20:59:51 -0500

    Adding details to LongestPrefix

--- LongestCommonPrefix.java
+++ LongestCommonPrefix.java
@@ -1,3 +1,14 @@
+/*
+PROBLEM --------------------------------------------------------------------------------------------------------------
+
+Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+
+SOLUTION ------------------------------------------------------------------------------------------------------------
+*/
+
+
 class Solution {
 
     public String FindLongest(String[] stringList){
@@ -48,3 +59,5 @@ class Solution {
 
     }
 }
+
+//Not Solved
