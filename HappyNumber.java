365271263baded5c6ebbd5e3058d2901aab62dd5
Kichiro Nakamoto <santiago.ruizm40@gmail.com>
2023-07-12 20:58:34 -0500

    Adding details to HappyNumber

--- HappyNumber.java
+++ HappyNumber.java
@@ -1,3 +1,21 @@
+
+/*
+PROBLEM --------------------------------------------------------------------------------------------------------------
+
+Write an algorithm to determine if a number n is happy.
+
+A happy number is a number defined by the following process:
+
+Starting with any positive integer, replace the number by the sum of the squares of its digits.
+Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+Those numbers for which this process ends in 1 are happy.
+Return true if n is a happy number, and false if not.
+
+SOLUTION ------------------------------------------------------------------------------------------------------------
+*/
+
+
+
 class Solution {
     public boolean isHappy(int n) {
         
@@ -23,3 +41,5 @@ class Solution {
 
     }
 }
+
+//Not Solved
